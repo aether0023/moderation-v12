b2233cbb5c596c35f678f688d3d3f18bea8a8671
aether0023 <71839333+aether0023@users.noreply.github.com>
2020-11-08 21:36:37 +0300

    Update sicil.js

--- Commands/sicil.js
+++ Commands/sicil.js
@@ -7,7 +7,7 @@ module.exports.execute = async (client, message, args) => {
 
     let embed = new MessageEmbed().setTitle(message.member.displayName, message.author.avatarURL({dynamic: true})).setFooter('Aether & Serendia').setColor("RANDOM").setTimestamp();
     let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-    let data = cdb.get(`sicil.${user.id}.${message.guild.id}`);
+    let data = cdb.get(`sicil.${user.id}.${message.guild.id}`) || [];
     let siralama = data.length > 0 ? data.map((value, index) => `\`${index+1}.\` [**${value.komut}**] ${client.users.cache.get(value.mod) || value.mod} tarafından **${value.sebep}** nedeniyle ${new Date(value.zaman).toTurkishFormatDate()} zamanında cezalandırılmış. (\`#${value.cezaID}\`)`).join("\n") : "Bu Üyenin Ceza Bilgisi Bulunamadı."
     message.channel.send(embed.setDescription(`${siralama}`));
 
