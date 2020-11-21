a30695adab22cd40afed41284078b8e34e1e9a3f
aether0023 <71839333+aether0023@users.noreply.github.com>
2020-11-21 15:37:37 +0300

    Update ban.js

--- Commands/ban.js
+++ Commands/ban.js
@@ -28,8 +28,8 @@ module.exports.execute = async (client, message, args) => {
     let victim = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     let victimfetch = client.users.fetch(args[0]);
     let reason = args.splice(1).join(" ");
-    if (message.member.roles.highest.position <= victim.roles.highest.position) return message.channel.send(embed.setDescription(`Bu üyenin yetkileri senden yüksek veya aynı yetkide olduğunuz için işlemi gerçekleştiremiyorum.`)).then(x => x.delete({timeout: 10000}));
     if (!victim || reason.length < 1) return message.channel.send(embed.setDescription(`Komutu doğru kullanmalısın! \`Örnek: ${ayar.prefix || ""}ban @üye [sebep]\``)).then(x => x.delete({timeout: 10000}));
+    if (message.member.roles.highest.position <= victim.roles.highest.position) return message.channel.send(embed.setDescription(`Bu üyenin yetkileri senden yüksek veya aynı yetkide olduğunuz için işlemi gerçekleştiremiyorum.`)).then(x => x.delete({timeout: 10000}));
     if (victim.user.bot) return message.channel.send(embed.setDescription(`Bu komutu botlar üzerinde kullanamazsın!`)).then(x => x.delete({timeout: 10000}));
     
     if (banLimit >= 5) {
